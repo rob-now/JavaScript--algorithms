0873d22def3a2061998c57134828243ed229a920
rob-now <rnowakowski.prv@gmail.com>
2018-01-22 12:55:28 +0100

    Updated reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,14 @@
 function reverseString(str) {
+    //Checking if str is a string
+    if (typeof str !== 'string') {
+        return "Not a string";
+    }
+    //If str is a string then this will be executed
     var strToArray = str.split("");
     var reversedArray = strToArray.reverse();
     var joinedString = reversedArray.join("");
     return joinedString;
 }
 
+//Pass the argument to check the code
 reverseString("Hello World! How are you?");
